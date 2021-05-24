e71fd9b82c1349b8c684f728aa3a3dabe8b6d5e0
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-05-24 16:00:34 +0700

    feat: add retrofit module

---
+++ app/src/main/java/com/example/cft_demo_kt/dependencies/ApiConstants.kt
@@ -0,0 +1,6 @@
+package com.example.cft_demo_kt.dependencies
+
+object ApiConstants {
+    const val BASE_URL: String = "https://www.cbr-xml-daily.ru"
+    const val READ_TIMEOUT: Long = 5000
+}
--- app/src/main/java/com/example/cft_demo_kt/dependencies/modules/RetrofitModule.kt
+++ app/src/main/java/com/example/cft_demo_kt/dependencies/modules/RetrofitModule.kt
@@ -1,7 +1,38 @@
 package com.example.cft_demo_kt.dependencies.modules
 
+import com.example.cft_demo_kt.data.remote.ApiService
+import com.example.cft_demo_kt.dependencies.ApiConstants.BASE_URL
+import com.example.cft_demo_kt.dependencies.ApiConstants.READ_TIMEOUT
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 class RetrofitModule {
+    @Provides
+    @Singleton
+    fun provideOkHttp(): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder()
+        val interceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        return okHttpClient
+            .addInterceptor(interceptor)
+            .readTimeout(READ_TIMEOUT, TimeUnit.MILLISECONDS)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideApiService(okHttpClient: OkHttpClient): ApiService =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(okHttpClient)
+            .build()
+            .create(ApiService::class.java)
 }
