7eedda7f8aceacf824f43d97d1230b6188af95a0
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-05-24 16:24:03 +0700

    feat: add Database module in dagger

--- app/src/main/java/com/example/cft_demo_kt/data/local/ExchangeRateDatabase.kt
+++ app/src/main/java/com/example/cft_demo_kt/data/local/ExchangeRateDatabase.kt
@@ -1,9 +1,11 @@
 package com.example.cft_demo_kt.data.local
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.example.cft_demo_kt.data.entities.ExchangeRate
+import com.example.cft_demo_kt.data.local.dao.ExchangeRateDao
 
 @Database(entities = arrayOf(ExchangeRate::class), version = 1)
-abstract class ExchangeRateDatabase {
-    abstract fun getExchangeRateDatabase()
+abstract class ExchangeRateDatabase: RoomDatabase() {
+    abstract fun getExchangeRateDao(): ExchangeRateDao
 }
--- app/src/main/java/com/example/cft_demo_kt/dependencies/modules/DatabaseModule.kt
+++ app/src/main/java/com/example/cft_demo_kt/dependencies/modules/DatabaseModule.kt
@@ -1,7 +1,21 @@
 package com.example.cft_demo_kt.dependencies.modules
 
+import android.app.Application
+import androidx.room.Room
+import com.example.cft_demo_kt.data.local.ExchangeRateDatabase
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class DatabaseModule {
+    @Provides
+    @Singleton
+    fun provideExchangeRateDatabase(application: Application) =
+        Room.databaseBuilder(application, ExchangeRateDatabase::class.java, "exchangeRate.db").build()
+
+    @Provides
+    @Singleton
+    fun provideExchangeRateDao(exchangeRateDatabase: ExchangeRateDatabase) =
+        exchangeRateDatabase.getExchangeRateDao()
 }
