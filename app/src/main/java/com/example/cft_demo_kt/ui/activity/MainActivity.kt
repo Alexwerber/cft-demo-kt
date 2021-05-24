b2eca69f7d73ccc7cbbf624b43167c33f27a2271
Хорошев Алексей Алексеевич <alexwerber42@gmail.com>
2021-05-24 15:49:43 +0700

    chore: add basic structure

--- app/src/main/java/com/example/cft_demo_kt/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/cft_demo_kt/ui/activity/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.cft_demo_kt
+package com.example.cft_demo_kt.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cft_demo_kt.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
